f576dff0782b15e30d6a3cd33315259489c1d70d
Aoi Tanaka <aoi.tanaka@studerande.karlskogafhs.se>
2023-08-04 16:13:48 +0200

    Add js file

---
+++ Day 4 - Array Cardio 1/arrayCardio.js
@@ -0,0 +1,108 @@
+const inventors = [
+      { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+      { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+      { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+      { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+      { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+      { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+      { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+      { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+      { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+      { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+      { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+      { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+    ];
+
+const people = [
+    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
+
+const transportation = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+
+// 1. Filter the list of inventors for those who were born in the 1500's
+const fifteens = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+
+console.table(fifteens)
+
+// 2. Give us an array of the inventors first and last names
+const fullName = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+
+console.log(fullName)
+
+// 3. Sort the inventors by birthdate, oldest to youngest
+const oldToYoung = inventors.sort((a, b) => a.year - b.year);
+
+console.table(oldToYoung)
+
+// 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, inventor) => {
+
+    return total + (inventor.passed - inventor.year);
+
+}, 0);
+
+console.log(totalYears)
+
+// 5. Sort the inventors by years lived
+const livedShortToLong = inventors.sort((a, b) => {
+
+    const lastOne = a.passed - a.year;
+    const nextOne = b.passed - b.year;
+
+    return lastOne - nextOne;
+
+});
+
+console.table(livedShortToLong)
+
+// 5.5 Give us an array of the inventors lifetime with full names
+const lifetimes = inventors.map(inventor => {
+
+    const lifetime = inventor.passed - inventor.year;
+    const fullName = `${inventor.first} ${inventor.last}`;
+
+    return `${fullName} : ${lifetime}`;
+
+});
+
+console.table(lifetimes)
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const links = Array.from(document.querySelectorAll('.mw-category a'));
+
+const boulevardsWithDe = links
+    .map(link => link.innerText)
+    .filter(boulevardsName => boulevardsName.includes('de'));
+
+console.table(boulevardsWithDe)
+
+// 7. Sort the people alphabetically by last name
+const sortPeople = people.sort((a, b) => {
+
+    const [aLast, aFirst] = a.split(', ');
+    const [bLast, bFirst] = b.split(', ');
+
+    return aLast > bLast ? 1 : -1;
+    
+});
+
+console.table(sortPeople)
+
+// 8. Sum up the instances of each of transportation
+const sumUpTransportation = transportation.reduce((obj, item) => {
+    
+    if( !obj[item] ) { obj[item] = 0; };
+
+    obj[item]++;
+
+    return obj;
+    
+}, {});
+
+console.log(sumUpTransportation)
