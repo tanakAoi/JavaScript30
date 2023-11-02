f49777c814c3ae5fd2f466c8c1acef0b33c31e04
Aoi Tanaka <aoitanaka.0523@gmail.com>
2023-11-02 10:56:35 +0100

    Add getVideo function

--- Day 19 - Webcam Fun/main.js
+++ Day 19 - Webcam Fun/main.js
@@ -5,22 +5,38 @@ const webCamApp = () => {
     const ctx = mainScreen.getContext('2d')
     const strip = document.querySelector('.strip')
 
+
     const getVideo = () => {
 
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
 
         .then(localMediaStream => {
-            
             liveScreen.srcObject = localMediaStream
             liveScreen.play()
-            console.log(liveScreen)
         })
 
         .catch((error) => {
             console.error(error)
         })
     }
-    getVideo() 
+    getVideo()
+    
+    const paintToCanvas = () => {
 
+        const width = liveScreen.videoWidth
+        const height = liveScreen.videoHeight
+        mainScreen.width = width
+        mainScreen.height = height
+        console.log(width, height)
+
+        setInterval(() => {
+            ctx.drawImage(liveScreen, 0, 0, width, height)
+        }, 16)
+
+    }
+    
+
+    
+    liveScreen.addEventListener('canplay', paintToCanvas)
 }
 webCamApp()
